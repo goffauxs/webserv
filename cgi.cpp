f0c7189c403a4c89c080b73b601e73551a8aeb5f
Julie Van Audenhaege <rvan-aud@fu-r9-p5.s19.be>
2022-04-22 17:59:33 +0200

    fixed read

--- cgi.cpp
+++ cgi.cpp
@@ -5,23 +5,17 @@
 #include <iostream>
 #include <unistd.h>
 
-void		fork_exec(std::string path,int fd[2], int fd_i[2], std::string body, char **env)
+void		fork_exec(std::string path, int fd[2], int fd_i[2], std::string body, char **env)
 {
 	char	*av[3];
 
 	av[0] = (char *)"python";
 	av[1] = (char *)"server/cgi-bin/test.py";//&path[0];
 	av[2] = NULL;
-	int i = 0;
-	while (env[i])
-	{
-		std::cout << "env[" << i << "] = " << env[i] << std::endl;
-		i++;
-	}
 	dup2(fd_i[0], 0);
 	dup2(fd[1], 1);
 	dup2(fd[0], 0);
-	write(1, body.c_str(), body.size() + 1);
+	// write(1, body.c_str(), body.size() + 1);
 	close(fd_i[0]);
 	close(fd_i[1]);
 	close(fd[0]);
@@ -32,25 +26,13 @@ void		fork_exec(std::string path,int fd[2], int fd_i[2], std::string body, char 
 	exit(1);
 }
 
-struct timeval	zero_time()
-{
-	struct timeval	ret;
-
-	ret.tv_sec = 0;
-	ret.tv_usec = 0;
-	return (ret);
-}
-
 std::string exec_cgi(std::string path, std::string body, Request const &req)
 {
 	int			fd[2];
 	int			fd_i[2];
-	char		tmp[11];
+	char		tmp[11] = {0};
 	std::string	return_string = "";
-	fd_set	sockets;
-	struct timeval	timer = zero_time();
 
-	FD_ZERO(&sockets);
 	pipe(fd);
 	pipe(fd_i);
 	int pid = fork();
@@ -62,21 +44,14 @@ std::string exec_cgi(std::string path, std::string body, Request const &req)
 	close(fd[1]);
 	close(fd_i[0]);
 	wait(0);
-	FD_SET(fd[0], &sockets);
 	int r = read(fd[0], tmp, 10);
-	std::cout << "tmp = " << tmp << std::endl;
-	while (r == 10)
+	return_string += std::string(tmp);
+	while (r > 0)
 	{
-		return_string += std::string(tmp);
-		int se = select(fd[0] + 1, &sockets, NULL, NULL, &timer);
-		timer = zero_time();
-		if (se > 0)
-			r = read(fd[0], tmp, 10);
-		else
-			r = -1;
+		r = read(fd[0], tmp, 10);
 		tmp[r] = 0;
-	}
-	if (r != -1)
 		return_string += std::string(tmp);
+	}
+	std::cout << "return of the string = " << return_string << std::endl;
 	return (return_string);
 }
