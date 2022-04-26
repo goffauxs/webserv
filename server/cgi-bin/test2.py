5fcfa8c370501aae8ab110fc08dee918daa82bb8
Victor D. Forget <viforget@pop-os.localdomain>
2022-04-26 09:11:05 +0200

    new python script

---
+++ server/cgi-bin/test2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python)
+
+# Import modules for CGI handling 
+import cgi, cgitb 
+
+# Create instance of FieldStorage 
+form = cgi.FieldStorage() 
+
+# Get data from fields
+user_name = form.getvalue('user_name')
+user_message  = form.getvalue('user_message')
+
+print ("Content-type:text/html\r\n\r\n")
+print ("<html>")
+print ("<head>")
+print ("<title>Hello - Second CGI Program</title>")
+print ("</head>")
+print ("<body>")
+print ("<h2>Hello %s %s</h2>" % (user_name, user_message))
+print ("</body>")
+print ("</html>")
