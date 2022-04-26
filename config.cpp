f39864d1af597029c0a59622b1abfc5712a635e5
Simon Goffaux <sgoffaux@student.s19.be>
2022-04-26 11:30:18 +0200

    WIP config

--- config.cpp
+++ config.cpp
@@ -3,6 +3,65 @@
 #include <fstream>
 #include <iostream>
 
+Config::Config(const std::string& path)
+{
+	std::ifstream ifs("default.conf", std::ifstream::in);
+
+	if (ifs.good())
+	{
+		if (has_uneven_brackets(ifs))
+			return ;
+		ifs.seekg(0, ifs.beg);
+		char* server_str;
+		std::ifstream::streampos begin = ifs.tellg();
+		std::ifstream::streampos end;
+		std::streamsize diff;
+		while (!ifs.eof())
+		{
+			advance_to_next_bracket(ifs);
+			if (ifs.eof())
+				break;
+			end = ifs.tellg();
+			diff = end - begin;
+			ifs.seekg(begin);
+			server_str = new char[diff + 1];
+			ifs.read(server_str, diff);
+			server_str[diff] = '\0';
+			std::string tmp(server_str);
+			_list.push_back(ServerConfig(tmp));
+			delete server_str;
+			begin = end;
+		}
+	}
+}
+
+int Config::has_uneven_brackets(std::ifstream& stream)
+{
+	std::stack<int> brackets;
+
+	size_t line_counter = 1;
+	char curr_char;
+	while (!stream.eof())
+	{
+		stream.get(curr_char);
+		if (curr_char == '{')
+			brackets.push(line_counter);
+		else if (curr_char == '}')
+		{
+			if (brackets.size())
+				brackets.pop();
+			else
+				throw BracketException(CLOSED, line_counter);
+		}
+		if (stream.peek() == 10)
+			line_counter++;
+	}
+	if (brackets.empty())
+		return 0;
+	else
+		throw BracketException(OPEN, line_counter);
+}
+
 void Config::advance_to_next_bracket(std::ifstream& stream)
 {
 	std::stack<bool> brackets;
@@ -17,7 +76,7 @@ void Config::advance_to_next_bracket(std::ifstream& stream)
 	if (server == "{")
 		brackets.push(true);
 	else
-		std::cerr << "Error: Unexpected token after server" << std::endl;
+		throw UnexpectedToken();
 	while (!stream.eof())
 	{
 		stream.get(curr_char);
--- config.hpp
+++ config.hpp
@@ -3,6 +3,10 @@
 
 #include "server_config.hpp"
 #include "utils.hpp"
+#include <list>
+
+#define OPEN true
+#define CLOSED false
 
 class Config
 {
@@ -11,9 +15,34 @@ public:
 	Config(const Config& other);
 	Config& operator=(const Config& other);
 
-	const ServerConfig& getServerConfig() const;
+	const ServerConfig& getServerConfig(const std::string& port) const;
+	class BracketException : public std::exception
+	{
+	private:
+		std::string _str;
+	public:
+		BracketException(bool open, size_t line)
+		{
+			_str = (open) ? "Error: Unclosed '{' on line " : "Error: Extra '}' on line ";
+			_str += line;
+		}
+		const char* what() const throw()
+		{
+			return _str.c_str();
+		}
+	};
+	class UnexpectedToken : public std::exception
+	{
+		const char* what() const throw()
+		{
+			return "Error: Unexpected token after server";
+		}
+	};
 private:
 	void advance_to_next_bracket(std::ifstream& stream);
+	int has_uneven_brackets(std::ifstream& stream);
+
+	std::list<ServerConfig> _list;
 };
 
 #endif /* CONFIG_HPP */
